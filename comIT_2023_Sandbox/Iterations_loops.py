6acfc8d0e5c49150496b492e80e3188f6ad51801
davidpal3c <davidpal3c@gmail.com>
2023-02-28 21:03:50 -0800

    feb28

--- comIT_2023_Sandbox/Iterations_loops.py
+++ comIT_2023_Sandbox/Iterations_loops.py
@@ -1,35 +1,18 @@
 # Let's make an algorithm that sums the first "N" even numbers. 
+def sum_n_even_numbers():
+# take text input from user and convert into integer
+    n = int(input("Enter the number of even numbers to sum: \n"))       # '\n'  or '\t'  o excape character telling to enter a new line/ or tab space (wihtout modifiying code text when pressing enter for example)
+    sum = 0
+    even_counter = 0
 
-algortihm sum_n_event_numbers
-
-    var Number: number, counter, sum, n 
-    
-    input(n)                # 2 
-    number = 0              # 2
-    sum = 0                 # 2
-    even_counter = 0        # 1
-
-    while even_counter < n : 
-        number = number + 2
-        even_counter = even+counter + 1  
-        sum = sum + number               
-        
-    end while
-
-    print("The sum of the first " + n + " event numbers was: " + sum)
-
-end algorithm 
-
-
-
-Assume N is 5
-
-
-
-# 1 2 3 4 5 -> two even numbers
-# 1 2 3 4 5 6 7 8 9 10 11 12 -> 5 even numbers
-#         2 4 6 8 10 -> sum 30 (5 even numbers)
-
+    while even_counter < n:
+        even_counter = even_counter + 1  
+        sum = sum + (even_counter * 2)           
+# formated string: clear
+    print(f"The sum of the first {n} even numbers was: {sum}")
+# Instead of the usual concatenated conversion
+#     print("The sum of the first " + n + " event numbers was: " + sum)
+sum_n_even_numbers
 
 
 
